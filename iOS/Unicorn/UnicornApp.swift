7559f3340a96b93f64213b7f822a49bbd9c5b6e8
Dan Spindelbauer <daniel@sphere.me>
2021-09-26 17:10:28 +0100

    WIP set up SphereStore

--- iOS/Unicorn/UnicornApp.swift
+++ iOS/Unicorn/UnicornApp.swift
@@ -1,12 +1,41 @@
+import AppCore
 import SwiftUI
 
 @main
 struct UnicornApp: App {
+    let sphereStore: SphereStore
+
+    init() {
+        let httpClient = AppCore.AgentHTTPClientImpl(defaultHeaders: [:], delegate: nil)
+
+        let logger = AppCore.Logger(
+            isErrorEnabled: true,
+            backend: AppCoreLoggingBackend()
+        )
+        let database = DefaultSqlDatabaseProvider(
+            logger: logger,
+            taskRunner: UIApplication.shared
+        )
+        let builder = AppCore.SphereStoreBuilder(
+            environmentType: .production,
+            storeActorBuilders: [],
+            sqlDatabaseProvider: database,
+            preferences: AppCorePreferenceStore(),
+            connectivityMonitor: httpClient,
+            logger: logger
+        )
+        self.sphereStore = builder.makeStore(gitHubAccessToken: "123") // TODO
+    }
+
     var body: some Scene {
         WindowGroup {
           TabView {
             NavigationView {
-              NotificationList()
+              NotificationList(
+                viewModel: .init(
+                    useCase: sphereStore.notificationListUseCase
+                )
+              )
             }
             .tabItem {
               Label("Notifications", systemImage: "sparkles")
