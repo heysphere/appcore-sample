3e0de879a9ddf132e805b30846b2b861dea6fead
Dan Spindelbauer <daniel@sphere.me>
2021-09-29 10:08:59 +0100

    Added NotificationInfoUseCase

---
+++ AppCore/appcore-api/src/commonMain/kotlin/me/sphere/appcore/usecases/NotificationInfoUseCaseImpl.kt
@@ -0,0 +1,38 @@
+package me.sphere.appcore.usecases
+
+import me.sphere.appcore.dataSource.DataSource
+import me.sphere.appcore.dataSource.singleDataSource
+import me.sphere.logging.Logger
+import me.sphere.network.ConnectivityMonitor
+import me.sphere.sqldelight.SqlDatabaseGateway
+import me.sphere.sqldelight.StoreScope
+import me.sphere.sqldelight.operations.OperationUtils
+import me.sphere.sqldelight.operations.notifications.FetchNotificationInfoOperation
+
+internal fun createNotificationInfoUseCase(
+    database: SqlDatabaseGateway,
+    operationUtils: OperationUtils,
+    connectivityMonitor: ConnectivityMonitor,
+    storeScope: StoreScope,
+    logger: Logger,
+) = object : NotificationInfoUseCase {
+    override fun info(id: String): DataSource<NotificationInfo> = singleDataSource(
+        operationUtils = operationUtils,
+        operation = FetchNotificationInfoOperation,
+        input = FetchNotificationInfoOperation.Input(
+            notificationId = id
+        ),
+        query = { id ->
+            // TODO
+        },
+        mapper = { notification ->
+            NotificationInfo(
+                notification.id,
+                notification.reason,
+                notification.title,
+                notification.repositoryFullName,
+                notification.subjectId
+            )
+        }
+    )
+}
