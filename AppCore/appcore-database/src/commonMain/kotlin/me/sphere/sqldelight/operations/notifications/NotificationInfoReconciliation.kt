00f43332a0a6f61be924674e97394e6536efe1d0
Dan Spindelbauer <daniel@sphere.me>
2021-09-29 10:12:49 +0100

    Replaced Reconciliation with FetchNotificationInfoOperation

--- AppCore/appcore-database/src/commonMain/kotlin/me/sphere/sqldelight/operations/notifications/NotificationInfoReconciliation.kt
+++
@@ -1,7 +0,0 @@
-package me.sphere.sqldelight.operations.notifications
-
-import me.sphere.sqldelight.operations.PagingReconciliationDefinition
-
-object NotificationInfoReconciliation: PagingReconciliationDefinition() {
-    override val identifier = "NotificationInfoPaging"
-}
